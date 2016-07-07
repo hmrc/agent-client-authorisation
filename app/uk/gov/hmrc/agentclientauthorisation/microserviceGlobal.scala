dc55c6179c25306531678b7188d11b3458d99c11
Ben Letton <ben.letton@digital.hmrc.gov.uk>
2016-07-07 09:39:41 +0100

    AOSS-1418 BL Setting the structure for wiring dependent services in the microservice

--- app/uk/gov/hmrc/agentclientauthorisation/microserviceGlobal.scala
+++ app/uk/gov/hmrc/agentclientauthorisation/microserviceGlobal.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.agentclientauthorisation
 
 import com.typesafe.config.Config
@@ -34,7 +50,7 @@ object MicroserviceAuthFilter extends AuthorisationFilter {
   override def controllerNeedsAuth(controllerName: String): Boolean = ControllerConfiguration.paramsForController(controllerName).needsAuth
 }
 
-object MicroserviceGlobal extends DefaultMicroserviceGlobal with RunMode {
+trait MicroserviceGlobal extends DefaultMicroserviceGlobal with RunMode with ServiceRegistry with ControllerRegistry {
   override val auditConnector = MicroserviceAuditConnector
 
   override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig(s"microservice.metrics")
@@ -44,4 +60,10 @@ object MicroserviceGlobal extends DefaultMicroserviceGlobal with RunMode {
   override val microserviceAuditFilter = MicroserviceAuditFilter
 
   override val authFilter = Some(MicroserviceAuthFilter)
+
+  override def getControllerInstance[A](controllerClass: Class[A]): A = {
+    getController(controllerClass)
+  }
 }
+
+object MicroserviceGlobal extends MicroserviceGlobal
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,8 +4,12 @@ resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.0.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "467a8b417f9d90ab863f3172f1bebe1d7c49706b")
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.8.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "0.11.0")
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.10")
+
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.5")
+
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
