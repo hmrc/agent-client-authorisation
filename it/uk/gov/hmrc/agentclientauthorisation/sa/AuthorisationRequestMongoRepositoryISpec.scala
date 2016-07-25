39d26613df38afb8498a588aa1d8df60f59b61d6
Ben Letton <ben.letton@digital.hmrc.gov.uk>
2016-07-25 20:42:25 +0100

    [BL] A)SS-1424 adding the ability to list reqests by client SaUtr

--- it/uk/gov/hmrc/agentclientauthorisation/sa/AuthorisationRequestMongoRepositoryISpec.scala
+++ it/uk/gov/hmrc/agentclientauthorisation/sa/AuthorisationRequestMongoRepositoryISpec.scala
@@ -20,7 +20,8 @@ import org.joda.time.DateTime
 import org.scalatest.Inside
 import org.scalatest.LoneElement._
 import org.scalatest.concurrent.Eventually
-import uk.gov.hmrc.agentclientauthorisation.model.{Accepted, AgentClientAuthorisationRequest, Pending, StatusChangeEvent}
+import reactivemongo.bson.BSONObjectID
+import uk.gov.hmrc.agentclientauthorisation.model._
 import uk.gov.hmrc.agentclientauthorisation.repository.AuthorisationRequestMongoRepository
 import uk.gov.hmrc.agentclientauthorisation.support.ResetMongoBeforeTest
 import uk.gov.hmrc.domain.{AgentCode, SaUtr}
@@ -34,19 +35,22 @@ class AuthorisationRequestMongoRepositoryISpec extends UnitSpec with MongoSpecSu
 
   private val now = DateTime.now()
 
-  def repository = new AuthorisationRequestMongoRepository() {
+  private def repository = new AuthorisationRequestMongoRepository() {
     override def withCurrentTime[A](f: (DateTime) => A): A = f(now)
   }
+
 
   "AuthorisationRequestRepository create" should {
 
     "create a new StatusChangedEvent of Pending" in {
 
-      val created: AgentClientAuthorisationRequest = await(repository.create(AgentCode("123"), SaUtr("123456789")))
+      val agentCode = AgentCode("1")
+      val saUtr = SaUtr("1A")
 
-      inside(created) { case event: AgentClientAuthorisationRequest =>
-        event.agentCode shouldBe AgentCode("123")
-          event.clientSaUtr shouldBe SaUtr("123456789")
+      inside(addRequest(agentCode -> saUtr)) {
+        case event: AgentClientAuthorisationRequest =>
+          event.agentCode shouldBe agentCode
+          event.clientSaUtr shouldBe saUtr
           event.regime shouldBe "sa"
           event.events.loneElement shouldBe StatusChangeEvent(now, Pending)
       }
@@ -56,9 +60,11 @@ class AuthorisationRequestMongoRepositoryISpec extends UnitSpec with MongoSpecSu
   "AuthorisationRequestRepository update" should {
 
     "create a new StatusChangedEvent" in {
-      val created: AgentClientAuthorisationRequest = await(repository.create(AgentCode("123"), SaUtr("123456789")))
 
-      inside(await(repository.update(created.id, Accepted))) {
+      val created = addRequest(AgentCode("2"), SaUtr("2A"))
+      val updated = update(created.id, Accepted)
+
+      inside(updated) {
         case AgentClientAuthorisationRequest(created.id, _, _, _, events) =>
           events shouldBe List(
             StatusChangeEvent(now, Pending),
@@ -68,39 +74,104 @@ class AuthorisationRequestMongoRepositoryISpec extends UnitSpec with MongoSpecSu
     }
   }
 
-  "AuthorisationRequestRepository list" should {
+  "AuthorisationRequestRepository listing by agentCode" should {
 
     "return previously created and updated elements" in {
-      await(Future.sequence(List(
-        repository.create(AgentCode("123"), SaUtr("1234567890")),
-        repository.create(AgentCode("123"), SaUtr("1234567891")).map(q => repository.update(q.id, Accepted))
-      )))
 
-      val list = await(repository.list(AgentCode("123")))
+      val agentCode = AgentCode("3")
+      val saUtr1 = SaUtr("3A")
+      val saUtr2 = SaUtr("3B")
 
-      inside(list(0)) {
-        case AgentClientAuthorisationRequest(_, AgentCode("123"), SaUtr("1234567890"), "sa", List(StatusChangeEvent(date, Pending))) =>
+      val requests = addRequests(agentCode -> saUtr1, agentCode -> saUtr2)
+      update(requests.last.id, Accepted)
+
+      val list = listByAgentCode(agentCode)
+
+      inside(list head) {
+        case AgentClientAuthorisationRequest(_, `agentCode`, `saUtr1`, "sa", List(StatusChangeEvent(date, Pending))) =>
           date shouldBe now
+        case e => e
       }
+
       inside(list(1)) {
-        case AgentClientAuthorisationRequest(_, AgentCode("123"), SaUtr("1234567891"), "sa", List(StatusChangeEvent(date1, Pending), StatusChangeEvent(date2, Accepted))) =>
+        case AgentClientAuthorisationRequest(_, `agentCode`, `saUtr2`, "sa", List(StatusChangeEvent(date1, Pending), StatusChangeEvent(date2, Accepted))) =>
           date1 shouldBe now
           date2 shouldBe now
       }
     }
 
     "return elements only for the given agent code" in {
-      await(Future.sequence(List(
-        repository.create(AgentCode("123A"), SaUtr("1234567890")),
-        repository.create(AgentCode("123B"), SaUtr("1234567891"))
-      )))
 
-      val list = await(repository.list(AgentCode("123B")))
+      addRequests(AgentCode("123A") -> SaUtr("1234567890"), AgentCode("123B") -> SaUtr("1234567891"))
 
-      inside(list.loneElement) {
+      inside( listByAgentCode(AgentCode("123B")) loneElement ) {
         case AgentClientAuthorisationRequest(_, AgentCode("123B"), SaUtr("1234567891"), "sa", List(StatusChangeEvent(date, Pending))) =>
           date shouldBe now
       }
     }
   }
+
+
+  "AuthorisationRequestRepository listing by clientSaUtr" should {
+
+    "return an empty list when there is no such SaUtr" in {
+
+      listByClientSaUtr(SaUtr("no-such-id")) shouldBe Nil
+    }
+
+    "return a single agent request" in {
+
+      val saUtr = SaUtr("4A")
+      val agentCode = AgentCode("4")
+
+      addRequest(agentCode, saUtr)
+
+      inside(listByClientSaUtr(saUtr) loneElement) {
+        case AgentClientAuthorisationRequest(_, `agentCode`, `saUtr`, "sa", List(StatusChangeEvent(date, Pending))) =>
+          date shouldBe now
+      }
+    }
+
+    "return a multiple requests for the same client" in {
+
+      val firstAgent = AgentCode("5")
+      val secondAgent = AgentCode("6")
+      val clientSaUtr = SaUtr("5A")
+
+      addRequests(
+        firstAgent -> clientSaUtr,
+        secondAgent -> clientSaUtr,
+        AgentCode("should-not-show-up") -> SaUtr("another-client")
+      )
+
+      val requests = listByClientSaUtr(clientSaUtr)
+
+      requests.size shouldBe 2
+
+      inside(requests head) {
+        case AgentClientAuthorisationRequest(_, `firstAgent`, `clientSaUtr`, "sa", List(StatusChangeEvent(date, Pending))) =>
+          date shouldBe now
+      }
+
+      inside(requests(1)) {
+        case AgentClientAuthorisationRequest(_, `secondAgent`, `clientSaUtr`, "sa", List(StatusChangeEvent(date, Pending))) =>
+          date shouldBe now
+      }
+    }
+  }
+
+
+  private type Request = (AgentCode, SaUtr)
+
+  private def addRequests(requests: Request*) = {
+    await(Future sequence requests.map { case (code: AgentCode, utr: SaUtr) => repository.create(code, utr) })
+  }
+
+  private def addRequest(requests: Request) = addRequests(requests) head
+
+  private def listByAgentCode(agentCode: AgentCode) = await(repository.list(agentCode))
+
+  private def listByClientSaUtr(saUtr: SaUtr) = await(repository.list(saUtr))
+
+  private def update(id:BSONObjectID, status:AuthorisationStatus) = await(repository.update(id, status))
 }
