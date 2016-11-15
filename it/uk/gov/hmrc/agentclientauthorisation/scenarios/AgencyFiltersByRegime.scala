617b1acdd8b25de383990f93bfc86f18c100cd70
Leigh Anderson <landerson@equalexperts.com>
2016-11-15 11:32:06 +0000

    [LA][APB-282] Test agency filtering by regime

---
+++ it/uk/gov/hmrc/agentclientauthorisation/scenarios/AgencyFiltersByRegime.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentclientauthorisation.scenarios
+
+import org.scalatest._
+import org.scalatest.concurrent.Eventually
+import uk.gov.hmrc.agentclientauthorisation.model.MtdClientId
+import uk.gov.hmrc.agentclientauthorisation.support._
+import uk.gov.hmrc.domain.AgentCode
+import uk.gov.hmrc.play.auth.microservice.connectors.Regime
+
+class AgencyFiltersByRegimeISpec extends FeatureSpec with ScenarioHelpers with GivenWhenThen with Matchers with MongoAppAndStubs with Inspectors with Inside with Eventually {
+
+  implicit val arn = RandomArn()
+  private implicit val agentCode = AgentCode("LMNOP123456")
+  val mtdClientId: MtdClientId = FakeMtdClientId.random()
+
+  feature("Agencies can filter")  {
+
+    scenario("on the regime of invitations") {
+      val agency = new AgencyApi(arn, port)
+      Given("An agent is logged in")
+      given().agentAdmin(arn, agentCode).isLoggedInWithSessionId().andHasMtdBusinessPartnerRecord()
+
+      When("An agent sends several invitations")
+      agencySendsSeveralInvitations(agency, MtdSaRegime)
+
+      Then("The agent filters by mtd-sa")
+      agencyFiltersByMtdSa(agency)
+
+      Then("The agent filters by mtd-vat")
+      agencyFiltersByMtdVat(agency)
+    }
+  }
+
+  def agencyFiltersByMtdSa(agency: AgencyApi) = {
+    val invitations = agency.sentInvitations(filteredBy = Seq("regime" -> MtdSaRegime))
+
+    invitations.numberOfInvitations shouldBe 2
+  }
+
+  def agencyFiltersByMtdVat(agency: AgencyApi) = {
+    val invitations = agency.sentInvitations(filteredBy = Seq("regime" -> "mtd-vat"))
+
+    invitations.numberOfInvitations shouldBe 0
+  }
+}
