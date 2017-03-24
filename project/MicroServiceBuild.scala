972508f5766faa63872e598a9d1fb5042205d1e7
Leigh Anderson <landerson@equalexperts.com>
2017-03-24 13:09:02 +0000

    [LA][APB-675] Upgrade microservice-bootstrap

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -21,7 +21,7 @@ private object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc" %% "play-reactivemongo" % "5.2.0",
     ws,
-    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.13.0",
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.14.0",
     "uk.gov.hmrc" %% "play-authorisation" % "4.3.0",
     "uk.gov.hmrc" %% "play-health" % "2.1.0",
     "uk.gov.hmrc" %% "play-url-binders" % "2.1.0",
