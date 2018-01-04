e12d6f249bbe0e63761265da6c012578f2a427bc
Artur Opala <opala.artur@gmail.com>
2018-01-04 15:32:03 +0000

    [APB-1813][AO] monitor DES connector plus New Year's HMRC source file header upgrade

--- app/uk/gov/hmrc/agentclientauthorisation/connectors/RelationshipsConnector.scala
+++ app/uk/gov/hmrc/agentclientauthorisation/connectors/RelationshipsConnector.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,4 +52,3 @@ class RelationshipsConnector @Inject() (@Named("relationships-baseUrl") baseUrl:
     new URL(afiBaseUrl, s"/agent-fi-relationship/relationships/agent/$arn/service/$service/client/$clientId")
   }
 }
-
