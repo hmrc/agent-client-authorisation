10fded971a7c7c7324dd4d514db3a8d8e99ae4f1
Artur Opala <opala.artur@gmail.com>
2018-01-05 11:58:58 +0000

    [APB-1813][AO] shorten api monitoring key names

--- app/uk/gov/hmrc/agentclientauthorisation/MicroserviceMonitoringFilter.scala
+++ app/uk/gov/hmrc/agentclientauthorisation/MicroserviceMonitoringFilter.scala
@@ -39,14 +39,15 @@ class MicroserviceMonitoringFilter @Inject()(metrics: Metrics, routes: Routes)
 object RoutesConverter {
   def keyToPatternMapping(routes: Routes, variables: Set[String] = Set.empty): Seq[(String, String)] = {
     routes.documentation.map {
-      case (_, route, _) => {
+      case (method, route, _) => {
         val r = route.replace("<[^/]+>", "")
         val key = stripInitialAndTrailingSlash(r).split("/").map(
           p => if (p.startsWith("$")) {
             val name = p.substring(1)
-            if (variables.contains(name)) s"{$name}" else name
+            if (variables.contains(name)) s"{$name}" else ""
           } else p).mkString("-")
         val pattern = r.replace("$", ":")
+        Logger.info(s"$key-$method -> $pattern")
         (key, pattern)
       }
     }
@@ -61,12 +62,6 @@ object RoutesConverter {
 abstract class MonitoringFilter(val keyToPatternMapping: Seq[(String, String)], override val kenshooRegistry: MetricRegistry)
                                (implicit ec: ExecutionContext)
   extends Filter with HttpAPIMonitor with MonitoringKeyMatcher {
-
-  {
-    val maxRouteLength = keyToPatternMapping.map { case (_, p) => p.length }.max
-    Logger.info(keyToPatternMapping.map { case (k, p) => s"${p.padTo(maxRouteLength, " ").mkString}\t->\t$k" }.mkString("\nMonitoring Filter rules:\n\t", "\n\t", "\n"))
-  }
-
 
   override def apply(nextFilter: (RequestHeader) => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
 
