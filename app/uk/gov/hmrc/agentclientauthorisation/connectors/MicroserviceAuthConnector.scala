623e3ba0da8361869ff82baceff14ae7ab65a90a
marianne pearson <marianne.pearson@digital.hmrc.gov.uk>
2018-11-12 16:59:02 +0000

    upgrade libraries versions, change to simple-reactivemongo-7.x.x

--- app/uk/gov/hmrc/agentclientauthorisation/connectors/MicroserviceAuthConnector.scala
+++ app/uk/gov/hmrc/agentclientauthorisation/connectors/MicroserviceAuthConnector.scala
@@ -17,18 +17,23 @@
 package uk.gov.hmrc.agentclientauthorisation.connectors
 
 import java.net.URL
-import javax.inject.{Inject, Named, Singleton}
 
+import com.typesafe.config.Config
+import javax.inject.{Inject, Named, Singleton}
+import play.api.Configuration
 import uk.gov.hmrc.auth.core._
 import uk.gov.hmrc.http.HttpPost
 import uk.gov.hmrc.play.http.ws.WSPost
 
 @Singleton
-class MicroserviceAuthConnector @Inject()(@Named("auth-baseUrl") baseUrl: URL) extends PlayAuthConnector {
+class MicroserviceAuthConnector @Inject()(@Named("auth-baseUrl") baseUrl: URL, config: Configuration)
+    extends PlayAuthConnector {
 
   override val serviceUrl = baseUrl.toString
 
   override def http = new HttpPost with WSPost {
     override val hooks = NoneRequired
+
+    override def configuration: Option[Config] = Some(config.underlying)
   }
 }
