70aaa9cd8c812d818a075bd18129c1e1fe994437
Paul Tan <20016950+JTY009@users.noreply.github.com>
2019-10-09 10:12:57 +0100

    [APB-4416][PT]: Update Tests for Canceled Authorisation Via Agent (#289)

--- it/uk/gov/hmrc/agentclientauthorisation/controllers/AgentCancelInvitationControllerISpec.scala
+++ it/uk/gov/hmrc/agentclientauthorisation/controllers/AgentCancelInvitationControllerISpec.scala
@@ -17,14 +17,18 @@
 package uk.gov.hmrc.agentclientauthorisation.controllers
 
 import akka.stream.Materializer
-import org.joda.time.{DateTime, LocalDate}
+import org.joda.time.{DateTime, DateTimeZone, LocalDate}
 import play.api.test.FakeRequest
 import uk.gov.hmrc.agentclientauthorisation.controllers.ErrorResults.{InvitationNotFound, NoPermissionOnAgency}
+import uk.gov.hmrc.agentclientauthorisation.model.ClientIdentifier.ClientId
 import uk.gov.hmrc.agentclientauthorisation.model._
 import uk.gov.hmrc.agentclientauthorisation.repository.{InvitationsRepository, InvitationsRepositoryImpl, MongoAgentReferenceRepository}
-import uk.gov.hmrc.agentmtdidentifiers.model.InvitationId
+import uk.gov.hmrc.agentclientauthorisation.support.TestHalResponseInvitation
+import uk.gov.hmrc.agentmtdidentifiers.model.{Arn, InvitationId}
+import uk.gov.hmrc.domain.TaxIdentifier
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class AgentCancelInvitationControllerISpec extends BaseISpec {
 
@@ -41,60 +45,99 @@ class AgentCancelInvitationControllerISpec extends BaseISpec {
     await(invitationsRepo.ensureIndexes)
   }
 
-  "PUT /agencies/:arn/invitations/sent/:invitationId/cancel" should {
+  case class TestClient(clientType: Option[String],
+                        service: Service,
+                        urlIdentifier: String,
+                        clientId: TaxIdentifier,
+                        suppliedClientId: TaxIdentifier)
 
+  val itsaClient = TestClient(personal, Service.MtdIt, "MTDITID", mtdItId, nino)
+  val irvClient = TestClient(personal, Service.PersonalIncomeRecord, "NI", nino, nino)
+  val vatClient = TestClient(personal, Service.Vat, "VRN", vrn, vrn)
+  val trustClient = TestClient(business, Service.Trust, "UTR", utr, utr)
+  val cgtClient = TestClient(business, Service.CapitalGains, "CGTPDRef", cgtRef, cgtRef)
+
+  val testClients = List(
+    itsaClient,
+    irvClient,
+    vatClient,
+    trustClient,
+    cgtClient)
+
+  def createInvitation(clientType: Option[String],
+                       service: Service,
+                       arn: Arn,
+                       clientId: ClientId,
+                       suppliedClientId: ClientId): Future[Invitation] = {
+    invitationsRepo.create(
+      arn,
+      clientType,
+      service,
+      clientId,
+      suppliedClientId,
+      Some(dfe),
+      DateTime.now(DateTimeZone.UTC),
+      LocalDate.now().plusDays(14))
+  }
+
+  trait StubSetup {
+    givenAuditConnector()
+    givenAuthorisedAsAgent(arn)
+    givenGetAgencyNameAgentStub
+  }
+
+  def runSuccessfulCancelledInvitation(testClient: TestClient): Unit = {
     val request = FakeRequest("PUT", "agencies/:arn/invitations/sent/:invitationId/cancel")
-    val clientIdentifier = ClientIdentifier("FOO", MtdItIdType.id)
+    val getResult = FakeRequest("GET", "agencies/:arn/invitations/sent/:invitationId")
 
-    "return 204 when an invitation is successfully cancelled" in {
-      givenAuditConnector()
-      givenAuthorisedAsAgent(arn)
-      givenGetAgencyNameAgentStub
+    s"return 204 when an invitation is successfully cancelled for ${testClient.service}" in new StubSetup {
 
-      val invitation = await(
-        invitationsRepo.create(
-          arn,
-          Some("personal"),
-          Service.MtdIt,
-          clientIdentifier,
-          clientIdentifier,
-          None,
-          DateTime.now(),
-          LocalDate.now()))
+      val invitation = await(createInvitation(testClient.clientType, testClient.service, arn, testClient.clientId, testClient.suppliedClientId))
 
       val response = controller.cancelInvitation(arn, invitation.invitationId)(request)
-
       status(response) shouldBe 204
+
+      val updatedInvitation = controller.getSentInvitation(arn, invitation.invitationId)(getResult)
+      status(updatedInvitation) shouldBe 200
+
+      val invitationStatus = jsonBodyOf(updatedInvitation).as[TestHalResponseInvitation].status
+
+      invitationStatus shouldBe Cancelled.toString
+    }
+  }
+
+  def runUnsuccessfulCanceledInvitation(testClient: TestClient): Unit = {
+    s"return NoPermissionOnAgency when the logged in arn doesn't not match the invitation for ${testClient.service}" in {
+
+      val request = FakeRequest("PUT", "agencies/:arn/invitations/sent/:invitationId/cancel")
+
+      givenAuditConnector()
+      givenAuthorisedAsAgent(arn2)
+      givenGetAgencyNameAgentStub
+
+      val invitation = await(createInvitation(testClient.clientType, testClient.service, arn, testClient.clientId, testClient.suppliedClientId))
+
+      val response = controller.cancelInvitation(arn2, invitation.invitationId)(request)
+
+      await(response) shouldBe NoPermissionOnAgency
+    }
+  }
+
+  "PUT /agencies/:arn/invitations/sent/:invitationId/cancel" should {
+
+    testClients.foreach { testClient =>
+      runSuccessfulCancelledInvitation(testClient)
+      runUnsuccessfulCanceledInvitation(testClient)
     }
 
     "return InvitationNotFound when there is no invitation to cancel" in {
+      val request = FakeRequest("PUT", "agencies/:arn/invitations/sent/:invitationId/cancel")
       givenAuditConnector()
       givenAuthorisedAsAgent(arn)
 
       val response = controller.cancelInvitation(arn, InvitationId("A7GJRTMY4DS3T"))(request)
 
       await(response) shouldBe InvitationNotFound
-    }
-
-    "return NoPermissionOnAgency when the logged in arn doesn't not match the invitation" in {
-      givenAuditConnector()
-      givenAuthorisedAsAgent(arn2)
-      givenGetAgencyNameAgentStub
-
-      val invitation = await(
-        invitationsRepo.create(
-          arn,
-          Some("personal"),
-          Service.MtdIt,
-          clientIdentifier,
-          clientIdentifier,
-          None,
-          DateTime.now(),
-          LocalDate.now()))
-
-      val response = controller.cancelInvitation(arn2, invitation.invitationId)(request)
-
-      await(response) shouldBe NoPermissionOnAgency
     }
   }
 }
