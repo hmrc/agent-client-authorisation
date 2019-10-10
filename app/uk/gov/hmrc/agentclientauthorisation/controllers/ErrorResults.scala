753cc1cdfa3ba523792dc7ab6d5362ef7ad25917
Marianne Pearson <32272884+mariannepearson0@users.noreply.github.com>
2019-10-10 16:25:48 +0100

    [APB-4428][MP] changing error code when a non client tries to access a client endpoint

--- app/uk/gov/hmrc/agentclientauthorisation/controllers/ErrorResults.scala
+++ app/uk/gov/hmrc/agentclientauthorisation/controllers/ErrorResults.scala
@@ -18,67 +18,68 @@ package uk.gov.hmrc.agentclientauthorisation.controllers
 
 import play.api.libs.json.Json.toJson
 import play.api.libs.json.{JsValue, Json, Writes}
+import play.api.mvc.Result
 import play.api.mvc.Results._
 
 object ErrorResults {
 
   case class ErrorBody(code: String, message: String)
 
-  implicit val errorBodyWrites = new Writes[ErrorBody] {
+  implicit val errorBodyWrites: Writes[ErrorBody] = new Writes[ErrorBody] {
     override def writes(body: ErrorBody): JsValue = Json.obj("code" -> body.code, "message" -> body.message)
   }
 
-  val GenericUnauthorized = Unauthorized(
+  val GenericUnauthorized: Result = Unauthorized(
     toJson(ErrorBody("UNAUTHORIZED", "Bearer token is missing or not authorized.")))
-  val AgentNotSubscribed = Forbidden(
+  val AgentNotSubscribed: Result = Forbidden(
     toJson(ErrorBody("AGENT_NOT_SUBSCRIBED", "The Agent is not subscribed to Agent Services.")))
-  val ClientRegistrationNotFound = Forbidden(
+  val ClientRegistrationNotFound: Result = Forbidden(
     toJson(ErrorBody("CLIENT_REGISTRATION_NOT_FOUND", "The Client's MTDfB registration was not found.")))
-  val ClientNinoNotFound = Forbidden(
-    toJson(ErrorBody("CLIENT_NINO_NOT_FOUND", "There must be a NINO in the client's user profile.")))
-  val NotAnAgent = Forbidden(toJson(ErrorBody("NOT_AN_AGENT", "The logged in user is not an agent.")))
-  val NoPermissionOnAgency = Forbidden(
+  val NotAClient: Result = Forbidden(
+    toJson(ErrorBody("NOT_A_CLIENT", "The logged in user must be a client to proceed.")))
+  val NoPermissionOnAgency: Result = Forbidden(
     toJson(
       ErrorBody(
         "NO_PERMISSION_ON_AGENCY",
         "The logged in user is not permitted to access invitations for the specified agency.")))
-  val NoPermissionOnClient = Forbidden(
+  val NoPermissionOnClient: Result = Forbidden(
     toJson(
       ErrorBody(
         "NO_PERMISSION_ON_CLIENT",
         "The logged in client is not permitted to access invitations for the specified client.")))
-  val PostcodeDoesNotMatch = Forbidden(
+  val PostcodeDoesNotMatch: Result = Forbidden(
     toJson(
       ErrorBody(
         "POSTCODE_DOES_NOT_MATCH",
         "The submitted postcode did not match the client's postcode as held by HMRC.")))
-  val DateOfBirthDoesNotMatch = Forbidden(
+  val DateOfBirthDoesNotMatch: Result = Forbidden(
     toJson(
       ErrorBody(
         "DATE_OF_BIRTH_DOES_NOT_MATCH",
         "The submitted date of birth did not match the client's date of birth as held by HMRC.")))
-  val VatRegistrationDateDoesNotMatch = Forbidden(
+  val VatRegistrationDateDoesNotMatch: Result = Forbidden(
     toJson(
       ErrorBody(
         "VAT_REGISTRATION_DATE_DOES_NOT_MATCH",
         "The submitted VAT registration date did not match the client's VAT registration date as held by HMRC.")))
-  val InvitationNotFound = NotFound(
+  val InvitationNotFound: Result = NotFound(
     toJson(ErrorBody("INVITATION_NOT_FOUND", "The specified invitation was not found.")))
-  val InvalidClientId = BadRequest(
+  val InvalidClientId: Result = BadRequest(
     toJson(ErrorBody("INVALID_CLIENT_ID", "The CLIENT_ID specified is not in a valid format")))
-  val InvitationExpired = Gone(toJson(ErrorBody("INVITATION_EXPIRED", "Invitation has expired")))
-  def nonUkAddress(countryCode: String) =
+  def nonUkAddress(countryCode: String): Result =
     NotImplemented(toJson(ErrorBody(
       "NON_UK_ADDRESS",
       s"This API does not currently support non-UK addresses. The client's country code should be 'GB' but it was '$countryCode'.")))
-  def invalidInvitationStatus(message: String) = Forbidden(toJson(ErrorBody("INVALID_INVITATION_STATUS", message)))
-  def unsupportedService(message: String) = NotImplemented(toJson(ErrorBody("UNSUPPORTED_SERVICE", message)))
-  def unsupportedClientIdType(message: String) = BadRequest(toJson(ErrorBody("UNSUPPORTED_CLIENT_ID_TYPE", message)))
-  def postcodeFormatInvalid(message: String) = BadRequest(toJson(ErrorBody("POSTCODE_FORMAT_INVALID", message)))
-  def postcodeRequired(service: String) =
+  def invalidInvitationStatus(message: String): Result =
+    Forbidden(toJson(ErrorBody("INVALID_INVITATION_STATUS", message)))
+  def unsupportedService(message: String): Result = NotImplemented(toJson(ErrorBody("UNSUPPORTED_SERVICE", message)))
+  def unsupportedClientIdType(message: String): Result =
+    BadRequest(toJson(ErrorBody("UNSUPPORTED_CLIENT_ID_TYPE", message)))
+  def postcodeFormatInvalid(message: String): Result = BadRequest(toJson(ErrorBody("POSTCODE_FORMAT_INVALID", message)))
+  def postcodeRequired(service: String): Result =
     BadRequest(toJson(ErrorBody("POSTCODE_REQUIRED", s"Postcode is required for service $service")))
 
-  val GenericForbidden = Forbidden(
+  val GenericForbidden: Result = Forbidden(
     toJson(ErrorBody("NO_PERMISSION", "The logged in user is not permitted to perform the operation.")))
 
 }
