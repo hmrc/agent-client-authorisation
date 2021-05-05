bb75eac48618d1aa0b4b44539cfa83382a7330ff
Barry Sims <barrysims@users.noreply.github.com>
2021-05-05 11:07:53 +0100

    [APB-5207][BS] Remove egregious implicit

--- app/uk/gov/hmrc/agentclientauthorisation/util/util.scala
+++ app/uk/gov/hmrc/agentclientauthorisation/util/util.scala
@@ -20,6 +20,6 @@ import scala.concurrent.Future
 
 package object util {
 
-  implicit def toFuture[A](a: A): Future[A] = Future.successful(a)
+  def toFuture[A](a: A): Future[A] = Future.successful(a)
 
 }
